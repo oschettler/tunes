c584aa6d11e28c54162161975e9297869d2bbd1c
Olav Schettler <olav@schettler.net>
2018-06-01 00:08:52 +0200

    Is startpage

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -6,6 +6,6 @@ class ProjectObserver
 {
     public function saving($project)
     {
-        Project::all()->update('is_startpage', false);
+        //Project::all()->update('is_startpage', false);
     }
 }
