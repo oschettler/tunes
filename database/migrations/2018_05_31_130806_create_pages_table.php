4c994bcb1c9791110c74611e8c14c38ce32d497d
Olav Schettler <olav@schettler.net>
2018-05-31 15:10:37 +0200

    Initial commit

---
+++ database/migrations/2018_05_31_130806_create_pages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->boolean('is_startpage')->default(false);
+            $table->boolean('is_public')->default(false);
+            $table->string('description')->nullable();
+            $table->text('markup');
+            $table->text('style')->nullable();
+            $table->text('script')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
