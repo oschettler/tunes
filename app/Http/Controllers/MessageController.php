5154f1685d5a033e8a7a7d60887a8f8221b426fa
Olav Schettler <olav@schettler.net>
2020-12-19 11:12:30 +0100

    Added timestamp

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,14 +2,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class MessageController extends Controller
 {
     public function update(Request $request)
     {
+        $now = Carbon::now()->toDateTimeString();
         $text = $request->input('text');
         file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/storage/nachricht.txt',
-            substr($text, 0, 200)
+            "{$now}|" . substr($text, 0, 200)
         );
         return back();
     }
